ae12a92fb37a8c86ccb26ed1d9f7b94ff248c349
Maxime Castres <mcastres@student.42.fr>
2020-04-21 15:30:55 +0200

    Upgrade to beta.20

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -29,7 +29,7 @@ const setDefaultPermissions = async () => {
     permissions.map(p =>
       strapi
         .query("permission", "users-permissions")
-        .update({ id: p.id }, { enabled: true })
+        .update({ id: p.id }, { enabled: false })
     )
   );
 };
@@ -85,36 +85,10 @@ const createSeedData = async () => {
   await Promise.all(articlesPromises);
 };
 
-const setDefaultFileUploader = async () => {
-  if (strapi.config.environment !== "production") {
-    return;
-  }
-  const pluginStore = strapi.store({
-    environment: strapi.config.environment,
-    type: "plugin",
-    name: "upload"
-  });
-  const config = await pluginStore.get({ key: "provider" });
-  await pluginStore.set({
-    key: "provider",
-    value: {
-      ...config,
-      ...{
-        provider: "cloudinary",
-        name: "Cloudinary",
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET
-      }
-    }
-  });
-};
-
 module.exports = async () => {
   const shouldSetDefaultPermissions = await isFirstRun();
   if (shouldSetDefaultPermissions) {
     await setDefaultPermissions();
-    await setDefaultFileUploader();
     await createSeedData();
   }
 };
