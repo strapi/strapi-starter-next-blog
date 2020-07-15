6c3cf117b86b9f9fa66d29c511ea8de73381fe7c
Chi Hao Poon <karvex@hotmail.com>
2020-07-15 21:51:41 +0200

    Refine KyberWidget

--- components/layout.js
+++ components/layout.js
@@ -13,6 +13,10 @@ const Layout = ({ children, categories }) => (
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
       />
+      <link
+        rel="stylesheet"
+        href="https://widget.kyber.network/v0.8.0/widget.css"
+      />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js" />
       <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js" />
