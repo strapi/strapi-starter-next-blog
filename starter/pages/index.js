e2cec182a8ac548a10da279d284b1b0b256f0678
Mark Kaylor <mark.kaylor@strapi.io>
2021-06-08 10:55:48 +0200

    use strapi draft and publish

--- starter/pages/index.js
+++ starter/pages/index.js
@@ -21,7 +21,7 @@ const Home = ({ articles, categories, homepage }) => {
 export async function getStaticProps() {
   // Run API calls in parallel
   const [articles, categories, homepage] = await Promise.all([
-    fetchAPI("/articles?status=published"),
+    fetchAPI("/articles"),
     fetchAPI("/categories"),
     fetchAPI("/homepage"),
   ]);
